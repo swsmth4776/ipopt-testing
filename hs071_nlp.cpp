c6cd09a804eb464210380507bf458fd8e1991736
Stan <swsmth476@gmail.com>
2020-05-25 22:13:59 -0700

    initial commit

---
+++ hs071_nlp.cpp
@@ -0,0 +1,5 @@
+//
+// Created by swsmth on 5/25/20.
+//
+
+#include "hs071_nlp.hpp"
---
+++ hs071_nlp.hpp
@@ -0,0 +1,14 @@
+//
+// Created by swsmth on 5/25/20.
+//
+
+#ifndef MYEXAMPLE_HS071_NLP_HPP
+#define MYEXAMPLE_HS071_NLP_HPP
+
+
+class hs071_nlp {
+
+};
+
+
+#endif //MYEXAMPLE_HS071_NLP_HPP
